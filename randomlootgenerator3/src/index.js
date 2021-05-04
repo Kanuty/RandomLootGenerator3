d4501570908f9ba422ea75a1ae9bb6bea3f64b89
Kanuty <bartoszdudek2211@gmail.com>
2021-05-04 19:33:46 +0200

    init
    Initial push
    added main libraries:
    - React
    - Redux
    - UI5

---
+++ randomlootgenerator3/src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import { store } from './app/store';
+import { Provider } from 'react-redux';
+import { ThemeProvider } from '@ui5/webcomponents-react';
+import * as serviceWorker from './serviceWorker';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
